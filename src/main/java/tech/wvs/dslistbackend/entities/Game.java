84554621940af004daddc156dc2ab0d684ec1201
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-22 15:46:16 -0300

    feat: add score and platforms fields to Game entity; create import.sql for initial data

--- src/main/java/tech/wvs/dslistbackend/entities/Game.java
+++ src/main/java/tech/wvs/dslistbackend/entities/Game.java
@@ -13,23 +13,30 @@ public class Game {
     private Long id;
     private String title;
 
+    private Double score;
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Double score, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
+        this.score = score;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -67,12 +74,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public Double getScore() {
+        return score;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getImgUrl() {
