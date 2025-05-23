b2c6425851cd39329980ecdbf6013506b3eb1f7c
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-23 18:11:11 -0300

    fix: correct SQL query string quotes in GameRepository. Project ready to homolog

--- src/main/java/tech/wvs/dslistbackend/repository/GameRepository.java
+++ src/main/java/tech/wvs/dslistbackend/repository/GameRepository.java
@@ -10,13 +10,13 @@ import java.util.List;
 public interface GameRepository extends JpaRepository<Game, Long> {
 
     @Query(nativeQuery = true, value = """
-		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS "year", tb_game.img_url AS imgUrl,
 		tb_game.short_description AS shortDescription, tb_belonging.position
 		FROM tb_game
 		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
 		WHERE tb_belonging.list_id = :listId
 		ORDER BY tb_belonging.position
-			""")
+		""")
     List<GameMinProjection> searchByList(Long listId);
 
     Long id(Long id);
